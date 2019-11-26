25ac798ade8b7797901a0f48238f19b64a567312
kook101 <jaekook.h@gmail.com>
2019-11-26 00:09:36 -0500

    Tiny steps_ d3.js_day1

---
+++ day1/day1.js
@@ -0,0 +1,10 @@
+//d3 DOM selector
+d3.select('h1').style('color','red')
+.attr('class', 'heading')
+.text('Updated h1 tag');
+
+d3.select('body').append('p').text('First Paragraph');
+d3.select('body').append('p').text('Second Paragraph');
+d3.select('body').append('p').text('Third Paragraph');
+
+d3.selectAll('p').style('color','blue');
