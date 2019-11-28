5fab298829468f2ec5000f35a8750babbcb3a350
kook101 <jaekook.h@gmail.com>
2019-11-28 00:36:45 -0500

    TinyStep_D3.js_Day3

---
+++ day3/day3.js
@@ -0,0 +1,20 @@
+//d3 array
+//statistics, search, transformations, histograms, histogram threshholds
+//d3 dsv
+//declarative programming - 선언형 프로그래밍
+//Tell d3 what you want
+
+//imperative programming - 명령형 프로그램
+//tell js how to do it
+
+//Data biding - array of numbers
+
+var dataSet = [ 1, 2, 3, 4, 5];
+
+d3.select('body')
+    .selectAll('p')
+    .data(dataSet)
+    .enter()
+    .append('p')//append paragraph for each data element
+    // .text('D3 Day2!');
+    .text(function(d){return d;});
